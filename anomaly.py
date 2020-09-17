38503ecef6e2657aa850a01d6ff44399e5c31c9d
21ChenYe <ce.chenye@gmail.com>
2020-09-17 17:19:11 -0400

    fixed anomaly.py

--- anomaly.py
+++ anomaly.py
@@ -39,29 +39,30 @@ def detection(data):
     print("Mean of Class1 data:", str(meanData))
     print("Variance of Class1 data:", str(stdData**2))
 
-
-    #Filtering the bad data by removing things 2 standard deviations above and below
+    #Find anomalies in the data
     goodData = classData[(classData <= (meanData + 2*stdData)) & (classData >= (meanData - 2*stdData))]
-    #Finding the median and variance of the filtered data
     goodMedian = goodData.median()
-    goodVar = goodData.var()
-    print("Mean of readjusted Class1 data:", str(goodMedian))
+    goodVar = goodData.std()**2
+    print("Median of readjusted Class1 data:", str(goodMedian))
     print("Variance of readjusted Class1 data:", str(goodVar))
     print("Percentage of total data points that have been excluded: ", str(100-len(goodData)/len(classData)*100))
     return goodData
+    
+    
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="load and analyse IoT JSON data")
     p.add_argument("file", help="path to JSON data file")
     P = p.parse_args()
-
+    
     file = Path(P.file).expanduser()
     #load our data
     data = load_data(file)
     goodData = detection(data)
     plt.figure()
-    goodData.plot.density()
-    plt.title("Probability Density Function of class 1 temperature (excluded values)")
+    goodData.plot.hist()
+    plt.title("Histogram of class 1 temperature (excluded values)")
     plt.xlabel("Temperature")
     plt.show()
+    
